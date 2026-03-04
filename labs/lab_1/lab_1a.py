3b606d717761237d98ef962c8d2a5670a805d71a
tiekeys <118081216+tiekeys@users.noreply.github.com>
2026-03-03 19:53:39 -0800

    add line to test pull request

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -5,6 +5,8 @@ The first lab in the BWSI CSS course. To complete this lab, fill out the variabl
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
 This is to simulate a change made on a robot: robot_speed = 5 # m/s
+
+test for last question
 """
 
 def main():
